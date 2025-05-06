039d87820f6d524dd83a6c051d964c7f0b0d184f
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-05-06 21:02:47 +0530

    add week 1 lecture notes

---
+++ 4COSC006C Software Development I/CW/Week 1/test 1.py
@@ -0,0 +1,49 @@
+##x=3
+##y=5
+##z=6
+##x=y+z+x
+##print(x)
+##a=2
+##b=3
+##a=b+a
+##b=a+a
+##print(a,b)
+##cost=5
+##factor=7
+##factor=factor*3
+##factor=factor+cost
+##print(factor)
+
+##num_1=21
+##num_2=10
+##num_3=num_1/num_2
+##num_4=num_1//num_2
+##print(num_4)
+
+##first = "sachintha"
+##second ="chamod"
+##third =first + ' ' + second
+##print(third*50)
+
+##a='she\'s mine,she said "hi !"'
+##print(a)
+
+##hello ='''this is one line.\n another line.'''
+##print(hello)
+##
+##a ='Where\'s the lecture room?'
+##print(a)
+##print()
+##print('Hello')
+##print(42)
+##greeting = 'Hello'
+##print(greeting)
+##print('dog','cat',42)
+##print('dog',end='')
+##print('cat')
+
+product = 'mask'
+price = 99
+print("the product:" + ' ' + product+ ' ' +"cost:" + ' ' + str(price)+' ' + "pence")
+print("The product:",product,"cost:",price,"pence")
+
