fffd6e79ac787745ad00d7a9bcac71726567f853
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-07-31 22:04:21 +0530

    docs: Add basics file

---
+++ 4COSC010C.2 Software Development/Cording/Basic/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;  //import scanner for get information from someone
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        int a = 20;//integer values
+        double b = 2461.2565;//float values
+        char c = 'H';
+        String d = "Hi Sachintha";
+
+        System.out.println("Hello and welcome!");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        */
+
+        /*
+        Scanner x = new Scanner(System.in);   // sacanner for get some infor from user
+        String name = x.nextLine();
+        System.out.println(name);
+
+        */ //Get string for input
+
+        //Get integer value for input
+        /*
+        Scanner x = new Scanner(System.in);
+
+        int my_age = x.nextInt();
+        System.out.println(my_age);
+        */ //get integer value
+
+        /*
+        Scanner x = new Scanner(System.in);
+
+        double A = x.nextDouble();
+        System.out.println(A);
+        */
+
+        
+        Scanner x = new Scanner(System.in);
+
+        double A = x.nextDouble();
+        System.out.println("Value is = " + A);
+
+    }
+}
