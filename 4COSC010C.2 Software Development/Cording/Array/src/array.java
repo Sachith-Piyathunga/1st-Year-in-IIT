57fa32e63afc4f1760fe12ed1fd18edc0f1c8fca
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-07-31 22:02:53 +0530

    docs: Add Software Development folder

---
+++ 4COSC010C.2 Software Development/Cording/Array/src/array.java
@@ -0,0 +1,30 @@
+
+public class array {
+
+    public static void main(String[] args) {
+        // basic array
+        /*
+        int[] numbers = {2,4,6,8};
+        String[] names = {"kamal", "nimal", "sunil"};
+
+        System.out.println(names[2]);*/
+
+        //1D array
+        /*
+        String[] name = new String[10];
+        name[0] = "kamal";
+        name[1] = "nimal";
+        name[2] = "sunil";
+        name[3] = "abc";
+        name[4] = "def";
+
+        System.out.println(name[3]);*/
+
+        //2D array
+        int[][] x = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}};
+        System.out.println(x[1][1]);
+    }
+}
