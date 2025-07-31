7f5b1fff990f03cce3bbbab2b0638c5f76c1e4c7
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-07-31 22:16:42 +0530

    docs: Add without loop file

---
+++ 4COSC010C.2 Software Development/Cording/withoutloop/src/Main.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void printNumbers(int n){
+        if(n<=10){
+            System.out.println(n);
+            printNumbers(n + 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        printNumbers(1);
+    }
+}
