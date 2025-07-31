c5fe42a075629b14ecf39b827d2e87794212793f
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-07-31 22:10:05 +0530

    docs: Add Mathematical Operators file

---
+++ 4COSC010C.2 Software Development/Cording/Mathematical Operators/src/Main.java
@@ -0,0 +1,29 @@
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        int a = 10;
+        int b = 5;
+        int x,y;
+        x = 53;
+        y = 26;
+        int z = ++x;
+        int c = ++x;
+        x+= 5;
+        x = x + 5;
+        x -= 5;
+        x = x - 5;
+
+        int answer = a + b * x / y; // if we change int to double we can get flot values as well
+
+        System.out.println("answer is = " + answer);
+        System.out.println("z is = " + z);
+        System.out.println("c is = " + c);
+        System.out.println("x is = " + x);
+}
+    }
