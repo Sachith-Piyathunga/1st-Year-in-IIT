71db26b3a84d16df12b1d517a7ce336b99f67928
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-07-31 22:08:36 +0530

    docs: Add if else file

---
+++ 4COSC010C.2 Software Development/Cording/if else/src/Main.java
@@ -0,0 +1,47 @@
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*int x = 10;
+        if (x == 10){ //equel operetor
+            System.out.println("Corret");
+
+            }
+        else{
+            System.out.println("u idiot");
+        }*/
+
+        /*int x = 10;
+        if (x != 80){ // not equl oporetor
+            System.out.println("Corret");
+
+        }
+        else{
+            System.out.println("u idiot");
+        }*/
+
+        /*int x = 10;
+        if (x > 10){ // greater
+            System.out.println("Corret");
+
+        }
+        else{
+            System.out.println("u idiot");
+        }*/
+
+        int x = 10;
+        if (x <= 10){ //less than or equl
+            System.out.println("Corret");
+
+        }
+        else{
+            System.out.println("u idiot");
+        }
+
+
+        }
+    }
+
