ad41e44a7d397fa787c95d05a3b7ab267d89c90d
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-07-31 22:06:14 +0530

    docs: Add Do while file

---
+++ 4COSC010C.2 Software Development/Cording/DO while/src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Hi Sachintha");
+            i++;
+        }
+        while(i<=10); //need semi colan
+
+    }
+}
