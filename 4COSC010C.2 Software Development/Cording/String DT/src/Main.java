354bda117aa3145c4c46732842a1ec6317d48e2d
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-07-31 22:11:34 +0530

    docs: Add String DT file

---
+++ 4COSC010C.2 Software Development/Cording/String DT/src/Main.java
@@ -0,0 +1,26 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        String a1 = "Sachintha";
+        String a2 = new String("Sachintha");
+
+        if (a2 == a1)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+        //System.out.println(a1);
+        //System.out.println(a2);
+        */
+
+        // create character array using string data type
+
+        char[] x = {'j','a','v','a'};
+
+        String srt1 = new String(x);
+
+        System.out.println(srt1);
+    }
+}
