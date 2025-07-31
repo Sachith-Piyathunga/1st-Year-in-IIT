e974d8b40ec999fed4645280a474bbc8cb3ad500
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-07-31 22:09:18 +0530

    docs: Add Logical operators file

---
+++ 4COSC010C.2 Software Development/Cording/Logical operators/src/Main.java
@@ -0,0 +1,35 @@
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // AND operator
+        /*
+        int x = 20;
+        int y = 10;
+
+        if (x>0 && y>0) {                     // AND operator sign is = &&
+
+            System.out.println("Positive");
+        }
+        else {
+            System.out.println("Negative");
+        } */
+
+        //OR operator
+
+        int x = 30;
+        int y = 20;
+
+        if (x>5 || y>10){
+
+            System.out.println("Correct");
+        }
+        else{
+            System.out.println("wrong");
+        }
+
+    }
+}
