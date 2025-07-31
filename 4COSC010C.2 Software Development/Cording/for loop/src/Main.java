4f8d372b7ce1b0182207f1c850295db3070ef432
Sachintha <138123891+Sachith-Piyathunga@users.noreply.github.com>
2025-07-31 22:07:23 +0530

    docs: Add for loop file

---
+++ 4COSC010C.2 Software Development/Cording/for loop/src/Main.java
@@ -0,0 +1,22 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //basic for loop
+        /*
+        for (int i = 1;i<=10;i++){
+
+            System.out.println("Hello");
+        }
+        */
+        String[] name = {"Kmal", "Nimal", "Sunil"};
+        for (int i = 0;i<=2;i++){
+
+            System.out.println(name[i]);
+
+        }
+
+    }
+}
